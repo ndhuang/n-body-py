f289a4ebc927a1b563f7ee423be52e15b4a70cdc
Nicholas Huang <ndhuang83706@gmail.com>
2013-05-02 21:52:02 -0600

    Final-ish format of Body.py.  Vector works, Body seems right, but is pending stress testing of the test algorithm

--- Body.py
+++ Body.py
@@ -1,31 +1,56 @@
 import numpy as np
 
 class Vector(np.ndarray):
-    # np.ndarray(stuff) != np.array(stuff)
-    # either need to use np.ndarray.new() or grab source code from np.array()
+    '''
+    A wrapper to np.ndarray that provides useful functions when dealing with
+    vectors (in the mathematical sense).
+    
+    '''
     def __new__(cls, *args):
+        # create a vector from an np.ndarray
         if len(args) > 1:
             return np.asarray(args).view(Vector)
         elif len(args) == 1:
             return np.asarray(*args).view(Vector)
         else:
-            # ooooh boy
-            assert(False)
+            return np.array(*args).view(Vector)
 
     def __mul__(self, other):
         if not isinstance(other, type(self)):
-            raise TypeError('__mul__ is not defined for %s and %s' 
-                            %(type(self), type(other)))
+            # if we're not multiplying with another Vector, default to 
+            # np.ndarray methods
+            return super(Vector, self).__mul__(other)
         return self.dot(other)
 
     def mag(self):
+        '''The magnitude of the vector (total length)'''
         return np.linalg.norm(self)
 
     def norm(self):
+        '''The normalized vector (same direction, magnitude = 1)'''
         return self / self.mag()
 
 class Body(object):
-    def __init__(self, m, x, y, z, v_x = 0, v_y = 0, v_z = 0):
+    '''
+    A body with position, velocity, acceleration and mass
+    '''
+    def __init__(self, m, x, y, z, 
+                 v_x = 0, v_y = 0, v_z = 0, 
+                 a_x = 0, a_y = 0, a_z = 0):
+        '''
+        Create the body.
+        
+        Parameters
+        ----------
+        m: float
+            mass, in kg
+        x, y, z: float
+            x, y, and z position in meters
+        v_x, v_y, v_z: float, optional
+            x, y, and z velocity in meters per second.  defaults to zero
+        a_x, a_y, a_z, float, optional
+            x, y, and z velocities in meters per second^2.  defaults to zero
+        '''
         self._G = 6.67e-11
         self.r = Vector(x, y, z)
         self.v = Vector(v_x, v_y, v_z)
@@ -33,12 +58,20 @@ class Body(object):
         self.m = np.float64(m)
 
     def displacement(self, other):
+        '''The vector displacement from `self` to `other`'''
         return Vector(other.r - self.r)
 
     def force(self, other):
+        '''
+        The vector force on `self` from `other`
+        other.force(self) == -self.force(other)
+        '''
         R = self.displacement(other)
         f = self.m * other.m * self._G / R.mag()**2
         return f * R.norm()
         
     def step(self, t):
+        '''
+        Step the body forward `t` seconds
+        '''
         self.r += self.a * t*t / 2 + self.v * t
